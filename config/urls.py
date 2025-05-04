16084dd8e43b51593cecf5c62bae5ed0a27539b0
Chika232010 <cyngyzmelisov96@gmail.com>
2025-05-04 14:27:57 +0500

    Add files via upload

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,views
+
+app_name = 'coupon'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('apply/', views.apply_coupon, name='apply_coupon'),
 ]
---
+++ coupons/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from .models import Coupon
+from .forms import CouponApplyForm
+
+def apply_coupon(request):
+    now = timezone.now()
+    form = CouponApplyForm(request.POST or None)
+    message = ""
+    
+    if request.method == 'POST' and form.is_valid():
+        code = form.cleaned_data['code']
+        try:
+            coupon = Coupon.objects.get(code__iexact=code, active=True,
+                                        valid_from__lte=now, valid_to__gte=now)
+            request.session['coupon_id'] = coupon.id
+            message = f"Купон {coupon.code} применён! Скидка {coupon.discount}%"
+        except Coupon.DoesNotExist:
+            request.session['coupon_id'] = None
+            message = "Купон недействителен"
+    
+    return render(request, 'coupon/apply.html', {'form': form, 'message': message})
